0cb0092b17e1da813988ae79e73b9b94adbd0427
Leandro Cieri <leandrocieris@gmail.com>
2021-01-22 17:26:36 +0000

    created class App for system control

---
+++ src/main/java/enviroment/ECUManagerLimitRPM.java
@@ -0,0 +1,29 @@
+package enviroment;
+
+public class ECUManager implements Actuator {
+
+    private int percentageThrottleIsOpen;
+    private int maxRPMs;
+
+    public void closeThrottlePartially(int percentageThrottleIsOpen){
+        if (percentageThrottleIsOpen >= 0 && percentageThrottleIsOpen <=100)
+            this.percentageThrottleIsOpen = percentageThrottleIsOpen;
+    }
+
+    public void limitRevs(int rpm){
+        maxRPMs = rpm;
+    }
+
+    public int getPercentageThrottleIsOpen() {
+        return percentageThrottleIsOpen;
+    }
+
+    public int getMaxRPMs() {
+        return maxRPMs;
+    }
+
+    @Override
+    public void execute(Object limitRPMs) {
+
+    }
+}
---
+++ src/main/java/enviroment/FallAllert.java
@@ -0,0 +1,2 @@
+package enviroment;public class FallAllert {
+}
